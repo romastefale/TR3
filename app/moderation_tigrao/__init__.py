402e9a03db2d4a0bc3b605ff33daa7407987ae5a
@tigrao <romastefale@gmail.com>
2026-05-12 20:49:54 -0300

    Export Tigrão DDX subrouter

--- app/moderation_tigrao/__init__.py
+++ app/moderation_tigrao/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from app.moderation_tigrao.ddx_router import router as ddx_router
 from app.moderation_tigrao.router import router
 
-__all__ = ["router"]
+__all__ = ["router", "ddx_router"]
