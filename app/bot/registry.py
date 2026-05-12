f8c5a49b2d96df7aa0f595ee0ed9d1c496984a4a
@tigrao <romastefale@gmail.com>
2026-05-12 16:57:52 -0300

    Add centralized handler registry

---
+++ app/bot/registry.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from aiogram import Dispatcher
+
+from app.bot.private_tools import router as private_router
+from app.bot.telegram import _register_handlers
+from app.handlers.lili_rodou import router as lili_rodou_router
+
+_registered = False
+
+
+def register_all_handlers(dispatcher: Dispatcher) -> None:
+    global _registered
+    if _registered:
+        return
+
+    dispatcher.include_router(private_router)
+    dispatcher.include_router(lili_rodou_router)
+    _register_handlers(dispatcher)
+    _registered = True
