d5b9678255158d356878d1802b5558a208bb15f0
@tigrao <romastefale@gmail.com>
2026-05-12 20:52:43 -0300

    Align Tigrão permissions with aiogram 3.27

--- app/moderation_tigrao/actions.py
+++ app/moderation_tigrao/actions.py
@@ -9,13 +9,19 @@ from aiogram.types import ChatPermissions
 def _full_permissions() -> ChatPermissions:
     return ChatPermissions(
         can_send_messages=True,
-        can_send_media_messages=True,
+        can_send_audios=True,
+        can_send_documents=True,
+        can_send_photos=True,
+        can_send_videos=True,
+        can_send_video_notes=True,
+        can_send_voice_notes=True,
         can_send_polls=True,
         can_send_other_messages=True,
         can_add_web_page_previews=True,
         can_change_info=False,
         can_invite_users=True,
         can_pin_messages=False,
+        can_manage_topics=False,
     )
 
 
